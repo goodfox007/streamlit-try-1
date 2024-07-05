ed02dfe04091f9a444785fb7fb6a40eea5379209
goodfox007 <34273039+goodfox007@users.noreply.github.com>
2024-07-05 19:09:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("First Edit!!!")
 st.write(
     "Let's  See[docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.image('/workspaces/streamlit-try-1/src/PKS_0587.jpg', caption='Baby and Chachu')
+st.image('/workspaces/streamlit-try-1/PKS_0587.jpg', caption='Baby and Chachu')
